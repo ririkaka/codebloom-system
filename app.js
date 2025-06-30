e22a6ed62fe8a5320baeb8797f0ceac81235f4dd
Nguyen Tu <thiennguyen0287@gmail.com>
2025-06-30 21:36:25 +0700

    Cài lại node_modules đúng môi trường Linux

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const axios = require('axios');
 
 const app = express();
