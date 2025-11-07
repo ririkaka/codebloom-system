8daf13ff473a1e048eb8a780d7e670978cb331a6
Nguyen Tu <thiennguyen0287@gmail.com>
2025-11-07 21:42:24 +0700

    Deploy CodeBloom system

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const mongoUri = process.env.MONGODB_URI;
 const JWT_SECRET = process.env.JWT_SECRET || 'secret-key';
 
