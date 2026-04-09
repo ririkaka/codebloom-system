79268ff8134325e21fff5ae964fda15c307a7e27
Nguyen Tu <thiennguyen0287@gmail.com>
2026-04-09 10:51:09 +0700

    add ping route

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/ping', (req, res) => {
+  res.status(200).send("pong");
+});
+
 // 🚀 PORT cho Render
 const PORT = process.env.PORT || 5000;
 
